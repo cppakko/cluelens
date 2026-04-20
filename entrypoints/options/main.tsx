7fe88cf0c6bec6996bc7f2f675a67ec110efa0fe
cppakko <a1378774701@gmail.com>
2026-04-20 22:57:44 +0800

    fix: isolate options entry assets from popup dictionary views

--- entrypoints/options/main.tsx
+++ entrypoints/options/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import '~/assets/globals.css';
 import { initI18n } from '@/i18n';
 
-initI18n().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
+if (location.pathname.endsWith('options.html')) {
+  initI18n().then(async () => {
+    const { default: App } = await import('./App.tsx');
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  });
+}
